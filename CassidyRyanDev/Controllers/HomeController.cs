0f814709120acbd3d248e41f9c6a08174c564055
CBullockRyan <cassidy.lynn.ryan@gmail.com>
2021-05-19 13:01:06 +0100

    add project files

---
+++ CassidyRyanDev/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CassidyRyanDev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Find me on Github or LinkedIn, or send me and email.";
+
+            return View();
+        }
+    }
+}
